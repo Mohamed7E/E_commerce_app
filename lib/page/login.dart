0372bed78d843cd80aeab3f6fb8c51830ff8d328
Mohamed7E <m.ibrahim.ict@btu.edu.eg>
2022-11-20 00:08:21 +0200

    create textboxe

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
+
+
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
@@ -6,11 +10,28 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-
-        Text("data")
-      ])
-      ,
+      body: Padding(
+        padding: const EdgeInsets.all(33.0),
+        child: Column(
+          children: [
+            SizedBox(height: 64,),
+          TextField(
+            keyboardType: TextInputType.text,
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: "enter your password : ",
+              enabledBorder: OutlineInputBorder(
+                borderSide: Divider.createBorderSide(context),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey),
+              ),
+              filled: true,
+              contentPadding: const EdgeInsets.all(8),
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
