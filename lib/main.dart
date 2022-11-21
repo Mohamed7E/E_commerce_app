26287af473bf12234097e904b8aa1dc7c48648cc
Mohamed7E <m.ibrahim.ict@btu.edu.eg>
2022-11-21 18:55:17 +0200

    create Register

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/page/login.dart';
+import 'package:flutter_application_1/page/register.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-       home: Login(),
+       home: Register(),
     );
   }
 }
---
+++ lib/page/register.dart
@@ -0,0 +1,85 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/shared/colors.dart';
+import 'package:flutter_application_1/shared/constants.dart';
+
+class Register extends StatelessWidget {
+  const Register({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(33.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment:MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 64,
+                ),
+                TextField(
+                  keyboardType: TextInputType.text,
+                  obscureText: false,
+                  decoration: decorationtextfiled.copyWith(
+                    hintText: "Enter username ",
+                  ),
+                ),
+                const SizedBox(
+                  height: 33,
+                ),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  obscureText: false,
+                  decoration: decorationtextfiled.copyWith(
+                    hintText: "Enter email ",
+                  ),
+                ),
+                const SizedBox(
+                  height: 33,
+                ),
+                TextField(
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  decoration: decorationtextfiled.copyWith(
+                    hintText: "Enter password ",
+                  ),
+                ),
+                const SizedBox(
+                  height: 33,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Regster",
+                    style: TextStyle(fontSize: 19),
+                  ),
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(BINgreen),
+                      padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)))),
+                ),
+                const SizedBox(
+                  height: 33,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("don't have account",style: TextStyle(fontSize: 18),),
+                    TextButton(onPressed: (){},
+                     child: Text("sign in",style: TextStyle(fontSize: 18),))
+                  ],
+                )
+              
+              
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
