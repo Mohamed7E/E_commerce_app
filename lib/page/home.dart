832cf2304aee1bdbbf5d49eb779524b0220374e6
Mohamed7E <m.ibrahim.ict@btu.edu.eg>
2022-11-21 21:09:30 +0200

    create GridView.builder

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -9,6 +9,19 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: GridView.builder(gridDelegate:
+       SliverGridDelegateWithFixedCrossAxisCount(
+
+        crossAxisCount: 2,
+        childAspectRatio: 3/2,
+        crossAxisSpacing:10  ,
+        mainAxisSpacing: 33,
+       
+       ),
+      itemCount: 4,
+      itemBuilder:(BuildContext  context,int index ){
+        return Container();
+      }),
       drawer: Drawer(
         child: Column(
           mainAxisAlignment:MainAxisAlignment.spaceBetween,
